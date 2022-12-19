497c844f0730bdaf6a420d87ee2b0ec785a7f773
VmTalik <vmarlyan@mail.ru>
2022-12-19 23:16:49 +0300

    Fix database class

--- db.py
+++ db.py
@@ -4,16 +4,17 @@ from mysql.connector import connect, Error
 class HospitalBase:
     def __init__(self):
         self.connection = None
-        self.connect()
 
-    def connect(self):
+    def connect(self, user_name='root', user_password=''):
         try:
             self.connection = connect(
                 host="localhost",
-                user="root",
+                user=user_name,
+                password=user_password,
                 database="Hospital",
                 port="3306"
             )
             print('Успешно подключились к базе данных')
         except Error as e:
             print(e)
+        return self.connection
